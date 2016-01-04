6f0525b9db40097accbb9237b81cfdf88b29b499
Kevin Martensson <kevinmartensson@gmail.com>
2016-01-04 20:20:10 +0100

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,50 +1,38 @@
-'use strict';
-var csvParser = require('csv-parser');
-var test = require('ava');
-var through = require('through2');
-var ifStream = require('./');
+import csvParser from 'csv-parser';
+import rfpify from 'rfpify';
+import test from 'ava';
+import through from 'through2';
+import fn from './';
 
 function isCsv(data) {
 	return data.toString().indexOf(',') !== -1;
 }
 
-test('meet condition', function (t) {
-	t.plan(2);
-
-	var stream = ifStream(isCsv, csvParser, {maxBuffer: 10000});
-
-	stream.on('data', function (data) {
-		t.assert(data.foo === 'bar');
-		t.assert(data.unicorn === 'cat');
-	});
-
+test('meet condition', async t => {
+	const stream = fn(isCsv, csvParser, {maxBuffer: 10000});
 	stream.end('foo,unicorn\nbar,cat\n');
+	const data = await rfpify(stream.once.bind(stream))('data');
+
+	t.is(data.foo, 'bar');
+	t.is(data.unicorn, 'cat');
 });
 
-test('doesn\'t meet condition', function (t) {
-	t.plan(1);
-
-	var stream = ifStream(isCsv, csvParser);
-
-	stream.on('data', function (data) {
-		t.assert(data.toString() === '{"hello":"world"}{"hello":"space"}');
-	});
-
+test('doesn\'t meet condition', async t => {
+	const stream = fn(isCsv, csvParser);
 	stream.end('{"hello":"world"}{"hello":"space"}');
+	const data = await rfpify(stream.once.bind(stream))('data');
+
+	t.is(data.toString(), '{"hello":"world"}{"hello":"space"}');
 });
 
-test('doesn\'t meet condition with a custom stream', function (t) {
-	t.plan(1);
-
-	var optionalStream = through(function (data, enc, cb) {
+test('doesn\'t meet condition with a custom stream', async t => {
+	const optionalStream = through((data, enc, cb) => {
 		cb(null, 'foobar');
 	});
 
-	var stream = ifStream(isCsv, csvParser, optionalStream);
-
-	stream.on('data', function (data) {
-		t.assert(data.toString() === 'foobar');
-	});
-
+	const stream = fn(isCsv, csvParser, optionalStream);
 	stream.end('{"hello":"world"}{"hello":"space"}');
+	const data = await rfpify(stream.once.bind(stream))('data');
+
+	t.is(data.toString(), 'foobar');
 });
