6ad55b160b06a36889e1c44f1fa08d213cb872f0
Kevin Martensson <kevinmartensson@gmail.com>
2016-05-06 09:20:36 +0200

    Improve tests

--- test.js
+++ test.js
@@ -1,38 +1,30 @@
 import csvParser from 'csv-parser';
-import rfpify from 'rfpify';
+import getStream from 'get-stream';
 import test from 'ava';
 import through from 'through2';
-import fn from './';
+import m from './';
 
-function isCsv(data) {
-	return data.toString().indexOf(',') !== -1;
-}
+const isCsv = data => data.toString().indexOf(',') !== -1;
 
 test('meet condition', async t => {
-	const stream = fn(isCsv, csvParser, {maxBuffer: 10000});
+	const stream = m(isCsv, csvParser, {maxBuffer: 10000});
 	stream.end('foo,unicorn\nbar,cat\n');
-	const data = await rfpify(stream.once.bind(stream))('data');
-
-	t.is(data.foo, 'bar');
-	t.is(data.unicorn, 'cat');
+	const data = await getStream.array(stream);
+	t.is(data[0].foo, 'bar');
+	t.is(data[0].unicorn, 'cat');
 });
 
 test('doesn\'t meet condition', async t => {
-	const stream = fn(isCsv, csvParser);
+	const stream = m(isCsv, csvParser);
 	stream.end('{"hello":"world"}{"hello":"space"}');
-	const data = await rfpify(stream.once.bind(stream))('data');
-
+	const data = await getStream(stream);
 	t.is(data.toString(), '{"hello":"world"}{"hello":"space"}');
 });
 
 test('doesn\'t meet condition with a custom stream', async t => {
-	const optionalStream = through((data, enc, cb) => {
-		cb(null, 'foobar');
-	});
-
-	const stream = fn(isCsv, csvParser, optionalStream);
+	const optionalStream = through((data, enc, cb) => cb(null, 'foobar'));
+	const stream = m(isCsv, csvParser, optionalStream);
 	stream.end('{"hello":"world"}{"hello":"space"}');
-	const data = await rfpify(stream.once.bind(stream))('data');
-
+	const data = await getStream(stream);
 	t.is(data.toString(), 'foobar');
 });
